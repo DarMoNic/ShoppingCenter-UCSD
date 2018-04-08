361da98e98f969c52484d92d17e450e518ad4b2b
Nicole Martindale <nicole@Nicoles-MacBook-Pro.local>
2018-04-08 02:35:24 -0700

    Initial Commit

---
+++ ShoppingCenter@UCSD/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShoppingCenter@UCSD
+//
+//  Created by Nicole Martindale on 4/8/18.
+//  Copyright © 2018 DarMoNic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
